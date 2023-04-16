4662aa1ac27bbe105ea3d24caa5ebb49e27e6bbe
ron-ligsay <akyligsay16@gmail.com>
2023-04-16 22:45:49 +0800

    variables and types

--- src/Mosh/Program.cs
+++ src/Mosh/Program.cs
@@ -15,6 +15,32 @@ namespace Mosh
         {
             // See https://aka.ms/new-console-template for more information
             Console.WriteLine("Hello, World!");
+            int Number = 1;
+            const float Pi = 3.14f;
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            // Naming Conventions
+            // Camel Case: firstName
+            // Pascal Case: FirstName
+            // Underscore: first_name
+            // Hungarian Notation: strFirstName
+            // For constant: Pascal Case - MaxZoom
+            float number = 1.2f;
+            decimal number2 = 1.2m;
+
+            //Non-Primitive Types
+            // String
+            // Array
+            // Enum
+            // Class
+
+            // Overflowing
+            byte number3 = 255; //byte can only hold 0-255
+            number3 = number3 + 1; // 0 because it overflows
+
+
         }
     }
 }
