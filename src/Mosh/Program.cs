7d2f0ebf3b0254db70a92958f1a30d15fdcaff5b
ron-ligsay <akyligsay16@gmail.com>
2023-04-17 09:42:37 +0800

    converting variables1

--- src/Mosh/Program.cs
+++ src/Mosh/Program.cs
@@ -14,12 +14,12 @@ namespace Mosh
         static void Main(string[] args)
         {
             // See https://aka.ms/new-console-template for more information
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine("Hello, World!");
             int Number = 1;
             const float Pi = 3.14f;
             byte b1 = 1;
-            int i = b1;
-            Console.WriteLine(Number + Pi + i);
+            int i1 = b1;
+            // Console.WriteLine(Number + Pi + i1);
 
             // Naming Conventions
             // Camel Case: firstName
@@ -29,8 +29,8 @@ namespace Mosh
             // For constant: Pascal Case - MaxZoom
             float number = 1.2f;
             decimal number2 = 1.2m;
-            Console.WriteLine("Floath {0}, Decimal {1}", number, number2);
-            //Non-Primitive Types
+            // Console.WriteLine("Floath {0}, Decimal {1}", number, number2);
+            // Non-Primitive Types
             // String
             // Array
             // Enum
@@ -57,15 +57,15 @@ namespace Mosh
                         // a = 1 is accesible anywhere in the code block
                         // b = 2 is accesible anywhere inside a 
                         // c = 3 is accesible only here
-                        Console.WriteLine("{0} {1} {2}", a, b, c);
+                        // Console.WriteLine("{0} {1} {2}", a, b, c);
                     }
                 }
             }
 
-            Console.WriteLine("Byte has a {0} to {1}", byte.MinValue, byte.MaxValue);
+            // Console.WriteLine("Byte has a {0} to {1}", byte.MinValue, byte.MaxValue);
             byte MinValue = 5;
             byte MaxValue = 55;
-            Console.WriteLine("My Byte has a {0} to {1}", MinValue, MaxValue);
+            // Console.WriteLine("My Byte has a {0} to {1}", MinValue, MaxValue);
 
 
             var number5 = 1; // var is a type that is inferred by the compiler // It detects what  kind of variable it is
@@ -75,40 +75,47 @@ namespace Mosh
             string firstName = "Mosh";
             bool isWorking = false;
 
-            Console.WriteLine(number5);
-            System.Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            // Console.WriteLine(number5);
+            // System.Console.WriteLine(count);
+            // Console.WriteLine(totalPrice);
+            // Console.WriteLine(character);
+            // Console.WriteLine(firstName);
+            // Console.WriteLine(isWorking);
 
-            // Constants
+            // // Constants
 
-            const float Pi = 3.14f;
-            Pi = 1; // Error because it is a constant
+            // const float Pi = 3.14f;
+            // Pi = 1; // Error because it is a constant
 
-            // Implicit Type  Conversion
-            byte b = 1;       //                            00000001
-            int i = b;        // 00000000 00000000 00000000 00000001
+            // // Implicit Type  Conversion
+            // byte b = 1;       //                            00000001
+            // int i = b;        // 00000000 00000000 00000000 00000001
 
-            // Explicit Type Conversion
-            int i = 1;
-            byte b = i; // Error because byte can only hold 0-255 // wont compile
-            // byte has a range of 0-255 // Data will be lost // It has to be explicit
-            byte b = (byte)i; // Casting
+            // // Explicit Type Conversion
+            // int i = 1;
+            // byte b = i; // Error because byte can only hold 0-255 // wont compile
+            // // byte has a range of 0-255 // Data will be lost // It has to be explicit
+            // byte b = (byte)i; // Casting
 
-            // another example
-            float f = 1.0f;
-            int i = (int)f; // 1
+            // // another example
+            // float f = 1.0f;
+            // int i = (int)f; // 1
 
-            // Non-compatible types
-            string s = "1";
-            int i = (int)s; // not compatible
-            int i = Convert.ToInt32(s); // using convert
-            int j = int.Parse(s); // using parse, it tries to convert it
+            // // Non-compatible types
+            // string s = "1";
+            // int i = (int)s; // not compatible
+            // int i = Convert.ToInt32(s); // using convert
+            // int j = int.Parse(s); // using parse, it tries to convert it
 
             // Convert
-            
+            try {
+                int i = 1;
+                byte b = (byte)i;
+                Console.WriteLine(b);
+            }
+            catch (Exception) {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
 
         }
     }
