21a4ed2d8bfeb3461d3ce7fe8c8abe57659e8bb4
ron-ligsay <akyligsay16@gmail.com>
2023-04-17 09:25:16 +0800

    printing variables

--- src/Mosh/Program.cs
+++ src/Mosh/Program.cs
@@ -17,9 +17,9 @@ namespace Mosh
             Console.WriteLine("Hello, World!");
             int Number = 1;
             const float Pi = 3.14f;
-            byte b = 1;
-            int i = b;
-            Console.WriteLine(i);
+            byte b1 = 1;
+            int i = b1;
+            Console.WriteLine(Number + Pi + i);
 
             // Naming Conventions
             // Camel Case: firstName
@@ -29,7 +29,7 @@ namespace Mosh
             // For constant: Pascal Case - MaxZoom
             float number = 1.2f;
             decimal number2 = 1.2m;
-
+            Console.WriteLine("Floath {0}, Decimal {1}", number, number2);
             //Non-Primitive Types
             // String
             // Array
@@ -37,14 +37,14 @@ namespace Mosh
             // Class
 
             // Overflowing
-            byte number3 = 255; //byte can only hold 0-255
-            number3 = number3 + 1; // 0 because it overflows
+            //byte number3 = 255; //byte can only hold 0-255
+            //number3 = number3 + 1; // 0 because it overflows
 
             // Using Checked
             checked // Like try catch or exception
             {
-                byte number4 = 255;
-                number4 = number4 + 1;
+                //byte number4 = 255;
+                //number4 = (byte)(number4 + 1);
             } // will not happen
 
             // Scope
@@ -57,10 +57,30 @@ namespace Mosh
                         // a = 1 is accesible anywhere in the code block
                         // b = 2 is accesible anywhere inside a 
                         // c = 3 is accesible only here
+                        Console.WriteLine("{0} {1} {2}", a, b, c);
                     }
                 }
             }
 
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            byte MinValue = 0;
+            byte MaxValue = 255;
+            Console.WriteLine("{0} {1}", MinValue, MaxValue);
+
+
+            byte number5 = 1;
+            int count = 10;
+            float totalPrice = 20.93f;
+            char character = 'A';
+            String firstName = "Mosh";
+            bool isWorking = false;
+
+            Console.WriteLine(number5);
+            System.Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
             
         }
     }
