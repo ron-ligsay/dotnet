735054d30ff150b1cc5130f97bf71fb2c05996f5
ron-ligsay <akyligsay16@gmail.com>
2023-05-10 22:28:03 +0800

    oop tutorial part 1

---
+++ oop with AvetisCodes/README.md
@@ -0,0 +1,9 @@
+
+
+Part 1 - Setup Visual Studio Code & .NET Core SDK
+https://www.youtube.com/watch?v=cL0OftSZ7V8&list=PLylgIRlJtHWRqlr1gtx3UgWXIzFS2ms9N
+
+
+
+
+
--- src/Mosh/Program.cs
+++ src/Mosh/Program.cs
@@ -14,12 +14,12 @@ namespace Mosh
         static void Main(string[] args)
         {
             // See https://aka.ms/new-console-template for more information
-            // Console.WriteLine("Hello, World!");
-            // int Number = 1;
-            // const float Pi = 3.14f;
-            // byte b1 = 1;
-            // int i1 = b1;
-            // Console.WriteLine(Number + Pi + i1);
+            Console.WriteLine("Hello, World!");
+            int Number = 1;
+            const float Pi = 3.14f;
+            byte b1 = 1;
+            int i1 = b1;
+            Console.WriteLine(Number + Pi + i1);
 
             // Naming Conventions
             // Camel Case: firstName
@@ -27,9 +27,9 @@ namespace Mosh
             // Underscore: first_name
             // Hungarian Notation: strFirstName
             // For constant: Pascal Case - MaxZoom
-            // float number = 1.2f;
-            // decimal number2 = 1.2m;
-            // Console.WriteLine("Floath {0}, Decimal {1}", number, number2);
+            float number = 1.2f;
+            decimal number2 = 1.2m;
+            Console.WriteLine("Floath {0}, Decimal {1}", number, number2);
             // Non-Primitive Types
             // String
             // Array
@@ -43,69 +43,69 @@ namespace Mosh
             // Using Checked
             checked // Like try catch or exception
             {
-                //byte number4 = 255;
-                //number4 = (byte)(number4 + 1);
+                byte number4 = 255;
+                number4 = (byte)(number4 + 1);
             } // will not happen
 
             // Scope
             {
-                // byte a = 1;
+                byte a = 1;
                 {
-                    // byte b = 2;
+                    byte b = 2;
                     {
-                        // byte c = 3;
+                        byte c = 3;
                         // a = 1 is accesible anywhere in the code block
                         // b = 2 is accesible anywhere inside a 
                         // c = 3 is accesible only here
-                        // Console.WriteLine("{0} {1} {2}", a, b, c);
+                        Console.WriteLine("{0} {1} {2}", a, b, c);
                     }
                 }
             }
 
-            // Console.WriteLine("Byte has a {0} to {1}", byte.MinValue, byte.MaxValue);
-            // byte MinValue = 5;
-            // byte MaxValue = 55;
-            // Console.WriteLine("My Byte has a {0} to {1}", MinValue, MaxValue);
+            Console.WriteLine("Byte has a {0} to {1}", byte.MinValue, byte.MaxValue);
+            byte MinValue = 5;
+            byte MaxValue = 55;
+            Console.WriteLine("My Byte has a {0} to {1}", MinValue, MaxValue);
 
 
-            // var number5 = 1; // var is a type that is inferred by the compiler // It detects what  kind of variable it is
-            // int count = 10;
-            // float totalPrice = 20.93f;
-            // char character = 'A';
-            // string firstName = "Mosh";
-            // bool isWorking = false;
+            var number5 = 1; // var is a type that is inferred by the compiler // It detects what  kind of variable it is
+            int count = 10;
+            float totalPrice = 20.93f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
 
-            // Console.WriteLine(number5);
-            // System.Console.WriteLine(count);
-            // Console.WriteLine(totalPrice);
-            // Console.WriteLine(character);
-            // Console.WriteLine(firstName);
-            // Console.WriteLine(isWorking);
+            Console.WriteLine(number5);
+            System.Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
 
             // // Constants
 
-            // const float Pi = 3.14f;
-            // Pi = 1; // Error because it is a constant
+            const float Pi = 3.14f;
+            Pi = 1; // Error because it is a constant
 
-            // // Implicit Type  Conversion
-            // byte b = 1;       //                            00000001
-            // int i = b;        // 00000000 00000000 00000000 00000001
+            // Implicit Type  Conversion
+            byte b = 1;       //                            00000001
+            int i = b;        // 00000000 00000000 00000000 00000001
 
-            // // Explicit Type Conversion
-            // int i = 1;
-            // byte b = i; // Error because byte can only hold 0-255 // wont compile
-            // // byte has a range of 0-255 // Data will be lost // It has to be explicit
-            // byte b = (byte)i; // Casting
+            // Explicit Type Conversion
+            int i = 1;
+            byte b = i; // Error because byte can only hold 0-255 // wont compile
+            // byte has a range of 0-255 // Data will be lost // It has to be explicit
+            byte b = (byte)i; // Casting
 
-            // // another example
-            // float f = 1.0f;
-            // int i = (int)f; // 1
+            // another example
+            float f = 1.0f;
+            int i = (int)f; // 1
 
-            // // Non-compatible types
-            // string s = "1";
-            // int i = (int)s; // not compatible
-            // int i = Convert.ToInt32(s); // using convert
-            // int j = int.Parse(s); // using parse, it tries to convert it
+            // Non-compatible types
+            string s = "1";
+            int i = (int)s; // not compatible
+            int i = Convert.ToInt32(s); // using convert
+            int j = int.Parse(s); // using parse, it tries to convert it
 
             // Convert
             try {
