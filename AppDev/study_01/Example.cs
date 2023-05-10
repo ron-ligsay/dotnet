239cdd3de1f361bfa57e005516311b2c556aad22
ron-ligsay <akyligsay16@gmail.com>
2023-05-11 00:24:58 +0800

    studies

---
+++ AppDev/study_01/Example.cs
@@ -0,0 +1,14 @@
+using System;
+using Acme.Collections;
+
+class Example {
+    public static void Main() {
+        var s = new Stack<int>();
+        s.Push(1);
+        s.Push(10);
+        s.Push(100);
+        Console.WriteLine(s.Pop());
+        Console.WriteLine(s.Pop());
+        Console.WriteLine(s.Pop());
+    }
+}
---
+++ AppDev/study_02/console.cs
@@ -0,0 +1,22 @@
+using  System;
+
+class Exercise
+{
+    private static void Main(string[] args)
+    {
+        variableName = Console.Read();
+
+
+        string FirstName;
+        Console.write("Enter First Name: ");
+        FirstName = console.ReadLine();
+
+        Console.WriteLine("Your First Name is { 0}", FirstName);
+        int Age = 15;
+        double salary = 22.74;
+        Console.WriteLine("Name: { 0}\n Age: { 1}\n Distance: { 2}",FirstName, Age, salary);
+        Console.WriteLine();
+
+        
+    }
+}
