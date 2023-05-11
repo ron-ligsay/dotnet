b85f578fc365bb0b6f6ec669f58a20b114beb576
ron-ligsay <akyligsay16@gmail.com>
2023-05-11 14:47:19 +0800

    duplicated

---
+++ oop with AvetisCodes/Part 3/Program copy.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace OOPExample 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(" ");
+        }
+    }
+
+    // Reserved Key Words
+    public class A 
+    {
+        
+    }
+
+    public abstract class B 
+    {
+
+    }
+
+    public interface C 
+    {
+
+    }
+}
