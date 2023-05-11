df15e25c39e123e56e3e9ac763a43ccc02e66deb
ron-ligsay <akyligsay16@gmail.com>
2023-05-11 14:29:27 +0800

    testiu

--- src/dotnet/TestUI/Program.cs
+++ src/dotnet/TestUI/Program.cs
@@ -1,6 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.Write("Please enter your first name: ");
-string firstName = Console.ReadLine();
-System.Console.WriteLine($"Hello {firstName}!");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Hello {firstName}!");
+    }
+}
