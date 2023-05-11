0aa8f3370e0bf9ab708c42e90ba724268721cd21
ron-ligsay <akyligsay16@gmail.com>
2023-05-11 14:42:32 +0800

    classes

---
+++ oop with AvetisCodes/Part 3/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace OOPExample {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" ");
+        }
+    }
+
+    public class A {
+
+    }
+
+    public abstract class B {
+
+    }
+
+    public interface C {
+        
+    }
+}
