c365ac3fd781d10901352356d60edaf8f16d352b
ron-ligsay <akyligsay16@gmail.com>
2023-05-11 15:01:23 +0800

    updates and build

--- oop with AvetisCodes/Part_3_OOPExample/Program.cs
+++ oop with AvetisCodes/Part_3_OOPExample/Program.cs
@@ -9,6 +9,16 @@ namespace Part_3_OOPExample
             //Console.WriteLine(" ");
             Laptop macbookAir = new MacbookAir();
             Laptop asusZenbook = new AsusZenbook();
+
+            macbookAir.TurnOn();
+            macbookAir.TurnOff();
+
+            asusZenbook.TurnOn();
+            asusZenbook.TurnOff();
+
+            Console.WriteLine("Macbook Air has touch screen: " + macbookAir.TouchScreen);
+            Console.WriteLine("Asus Zenbook has touch screen: " + asusZenbook.TouchScreen);
+
         }
     }
 
