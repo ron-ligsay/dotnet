40e86b16722a6f97354b8141aa720add230d9caa
ron-ligsay <akyligsay16@gmail.com>
2023-06-18 22:47:00 +0800

    readline

--- src/dotnet/TestUI/Program.cs
+++ src/dotnet/TestUI/Program.cs
@@ -5,7 +5,10 @@
         Console.WriteLine("Hello, World!");
 
         Console.Write("Please enter your first name: ");
-        string firstName = Console.ReadLine();
+        string firstName;
+        //  readline with exception
+        
+        firstName = Console.ReadLine();
         Console.WriteLine($"Hello {firstName}!");
     }
 }
